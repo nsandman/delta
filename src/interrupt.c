55d63262d4f435fddf3e63a5ca4cc25a5856fbe7
Noah Sandman <nsandman09@gmail.com>
2015-06-19 15:30:39 -0700

    Deleted demo.exdf

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,7 +1,7 @@
 #include <interrupt.h>
 
 void isr_handler(registers_t regs) {
-	printf("Got interrupt $%d!", regs.int_no);
+	printf("Got interrupt %u!", regs.int_no);
 	for(;;)
 		__asm__("hlt");
 }
